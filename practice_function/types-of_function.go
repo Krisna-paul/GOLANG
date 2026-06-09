c740637db36a8f5343aea1ced5975afe1e8f15a3
krisna Paul <krisna.2008034.ruet.mte@gmail.com>
2026-06-09 23:27:09 +0600

    --

--- practice_function/types-of_function.go
+++ practice_function/types-of_function.go
@@ -9,8 +9,17 @@ func main() {
 		c := a + b
 		fmt.Println("the sum is ", c)
 	}(10, 24)
+
+	mul := func(a int, b int) int {
+		return a * b
+	}
+	fmt.Println("the product is ", mul(10, 24))
 }
 
 func init() {
 	fmt.Println("I'll be invoked before the main function")
+}
+
+func multiply(a int, b int) int {
+	return a * b
 }
